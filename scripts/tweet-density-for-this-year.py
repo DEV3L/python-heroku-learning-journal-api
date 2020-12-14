755f7200fe68ec1a0ba7aca0fdf96451b2f94662
Justin Beall <jus.beall@gmail.com>
2020-12-13 19:42:02 -0500

    Finish tweet density script

--- scripts/tweet-density-for-this-year.py
+++ scripts/tweet-density-for-this-year.py
@@ -24,14 +24,38 @@ classifications = {
     CLASSIFICATION_OTHER: 0
 }
 
+DEFAULT_KCV_VALUE = 2
+
+WORDS_PER_PAGE = 250
+READ_WORDS_PER_MINUTE = 150
+
 
 def reduce_classifications(result: dict, tweet_event: TweetEventModel) -> dict:
     classification_extractor = ClassificationExtractor(tweet_event)
     classification = classification_extractor.classify()
 
-    result[classification] = result[classification] + 1
+    kcv_value = DEFAULT_KCV_VALUE
+    if "^" in tweet_event.text:
+        kcv_annotation = tweet_event.text[tweet_event.text.index("^") + 1:].split("\n")[0]
+
+        if kcv_annotation.endswith("m"):
+            kcv_value = int(kcv_annotation.replace("m", ""))
+        elif kcv_annotation.endswith("p"):
+            pages = int(kcv_annotation.replace("p", ""))
+            kcv_value = (pages * WORDS_PER_PAGE) / READ_WORDS_PER_MINUTE
+
+    result[classification] = result[classification] + kcv_value
 
     return result
+
+
+def hashtags_from_tweet(tweet: dict):
+    return [hashtag_entity['text'].lower()
+            for hashtag_entity in tweet['tweet']['entities']['hashtags']]
+
+
+def full_text_from_tweet(tweet: dict):
+    return tweet['tweet']['full_text']
 
 
 if __name__ == "__main__":
@@ -41,12 +65,11 @@ if __name__ == "__main__":
     time_extractor = TimeExtractor(data)
     tweets_from_this_year = time_extractor.tweets_for_year(current_year)
 
-    tweet_hashtags_from_this_year = [
-        TweetEventModel([hashtag_entity['text'].lower()
-                         for hashtag_entity in tweet['tweet']['entities']['hashtags']], tweet['tweet']['full_text'])
+    tweet_events_from_this_year = [
+        TweetEventModel(hashtags_from_tweet(tweet), full_text_from_tweet(tweet))
         for tweet in tweets_from_this_year]
 
-    classified_tweets = reduce(reduce_classifications, tweet_hashtags_from_this_year, classifications)
+    classified_tweets = reduce(reduce_classifications, tweet_events_from_this_year, classifications)
 
     for key, value in classified_tweets.items():
         print(f'{key}: {value}')
