08aa4b9e543bfbcd5483ec6a0f312b4e31a1995b
Justin Beall <jus.beall@gmail.com>
2021-01-02 19:17:54 -0500

    Handle adminsdk as a secret

--- src/dao/firebase.py
+++ src/dao/firebase.py
@@ -9,14 +9,15 @@ FIREBASE_DATABASE_URL = os.environ.get("FIREBASE_DATABASE_URL", "database_url")
 FIREBASE_STORAGE_BUCKET = os.environ.get("FIREBASE_STORAGE_BUCKET", "storage_bucket")
 FIREBASE_ADMIN_PRIVATE_KEY = os.environ.get("FIREBASE_ADMIN_PRIVATE_KEY", "admin_private_key")
 
-SERVICE_ACCOUNT_PATH = "./data/social-learning-journal-firebase-adminsdk.json"
+SERVICE_ACCOUNT_PATH = "./data/firebase-adminsdk.json"
+SERVICE_ACCOUNT_PATH_TEMP = "./data/_firebase-adminsdk.json"
 
 config = {
     "apiKey": FIREBASE_API_KEY,
     "authDomain": FIREBASE_AUTH_DOMAIN,
     "databaseURL": FIREBASE_DATABASE_URL,
     "storageBucket": FIREBASE_STORAGE_BUCKET,
-    "serviceAccount": SERVICE_ACCOUNT_PATH
+    "serviceAccount": SERVICE_ACCOUNT_PATH_TEMP
 }
 
 _firebase = None
@@ -33,10 +34,11 @@ def firebase_instance():
 
     with open(SERVICE_ACCOUNT_PATH) as service_account_file:
         service_account_json = json.load(service_account_file)
+
     service_account_json['private_key'] = firebase_admin_private_key
 
-    with open(SERVICE_ACCOUNT_PATH, 'w') as service_account_file:
-        json.dump(service_account_json, service_account_file)
+    with open(SERVICE_ACCOUNT_PATH_TEMP, 'w') as temp_service_account_file:
+        json.dump(service_account_json, temp_service_account_file)
 
     _firebase = pyrebase.initialize_app(config)
     return _firebase
