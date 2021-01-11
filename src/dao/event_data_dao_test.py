a9a1d5c899244599bd9fd418577ab722555808ab
Justin Beall <jus.beall@gmail.com>
2021-01-10 19:05:13 -0500

    Update event data model

--- src/dao/event_data_dao_test.py
+++ src/dao/event_data_dao_test.py
@@ -22,5 +22,72 @@ def test_event_data_dao_inserts_record(mongo_db):
     assert event_data_id
 
 
+def test_event_data_dao_does_not_update_record_with_invalid_identifier(mongo_db):
+    # event_data_model = _build_event_data_model()
+    # event_data_dao = EventDataDao(mongo_db)
+    #
+    # result = event_data_dao.update(event_data_model)
+    #
+    # assert not result
+    pass
+
+
+#
+#
+# def test_event_data_dao_does_update_record(mongo_db):
+#     pass
+#
+#
+# def test_event_data_dao_does_not_find_record(mongo_db):
+#     version_dao = VersionDao(mongo_db)
+#     result = version_dao.find_one(default_version)
+#
+#     assert result is None
+#
+#
+# def test_event_data_dao_does_find_one_record(mongo_db):
+#     version = _build_version_model()
+#     version_dao = VersionDao(mongo_db)
+#
+#     expected_version_id = version_dao.insert(version)
+#
+#     result_version = version_dao.find_one(default_version)
+#
+#     # cleanup
+#     assert version_dao.delete_one(default_version)
+#
+#     assert expected_version_id == str(result_version._id)
+#
+#
+# def test_event_data_dao_does_find_multiple_records(mongo_db):
+#     version = _build_version_model()
+#     version_dao = VersionDao(mongo_db)
+#
+#     expected_version_id = version_dao.insert(version)
+#
+#     result_version = version_dao.find_one(default_version)
+#
+#     # cleanup
+#     assert version_dao.delete_one(default_version)
+#
+#     assert expected_version_id == str(result_version._id)
+#
+#
+# def test_event_data_dao_delete_no_record(mongo_db):
+#     version_dao = VersionDao(mongo_db)
+#     deleted_count = version_dao.delete_one(default_version)
+#     assert 0 == deleted_count
+#
+#
+# def test_event_data_dao_deletes_record(mongo_db):
+#     version = _build_version_model()
+#     version_dao = VersionDao(mongo_db)
+#     version_dao.insert(version)
+#
+#     deleted_count = version_dao.delete_one(default_version)
+#
+#     assert 1 == deleted_count
+#
+
 def _build_event_data_model():
     return EventDataModel(source, data)
--- src/models/event_data_model.py
+++ src/models/event_data_model.py
@@ -2,9 +2,24 @@ from typing import Dict
 
 
 class EventDataModel:
-    def __init__(self, source: str, data: Dict):
+    def __init__(self, source: str, data: Dict, source_id: str = None, version: int = 0):
         self.source = source
+        self.source_id = source_id
+        self.version = version
         self.data = data
+
+        self.user = None
+
+        self.tags = []
+        self.images = []
+
+        self.author = None
+
+        self.raw_text = None
+        self.text = None
+        self.medium = None
+        self.classification = None
+        self.kcv = None
 
     @property
     def to_json(self) -> Dict:
